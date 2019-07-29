ed7e08ec7697fbea142ff4c78f8ca0c864882d73
“wszuwar <msuwr315@gmail.com>
2019-07-29 18:47:10 +0200

    Waste Repo

--- src/main/java/com/crud/orders/repository/WasteRepository.java
+++ src/main/java/com/crud/orders/repository/WasteRepository.java
@@ -3,6 +3,19 @@ package com.crud.orders.repository;
 import com.crud.orders.model.Waste;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WasteRepository extends JpaRepository<Waste, Long> {
 
+    @Override
+    List<Waste> findAll();
+
+    @Override
+    Waste findOne(Long id);
+
+    @Override
+    Waste save(Waste waste);
+
+    @Override
+    void delete(Waste waste);
 }
